e8d2a19191ba1682a3a8fd9e22a7e4c66fb74ac2
Rosita815 <rositajarquin26@gmail.com>
2024-08-01 19:49:41 -0600

    Feat-Entregable3

--- js/Main.js
+++ js/Main.js
@@ -1,6 +1,17 @@
 console.log("Funciona")
 
 /* Los componentes */
+document.addEventListener('DOMContentLoaded', ev => {
+    MostrarHTML()
+})
+/* 0- */
+function MostrarHTML() {
+
+    let navbar = document.getElementById("Navbar").innerHTML = CrearNavbar();
+    let footer = document.getElementById("Footer").innerHTML = CrearFooter();;
+    let Noticias = document.getElementById("Noticias").innerHTML = CrearTarjetaNoticias();;
+    
+}
 /* 1-Navbar */
 function CrearNavbar() {
     let html = `

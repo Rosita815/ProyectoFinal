48762df9fd36f4ec6eb584636a11e38360205010
Rosita815 <rositajarquin26@gmail.com>
2024-08-06 20:58:22 -0600

    Feat-Entregable_05 Fecha 06-08-2024

--- js/Main.js
+++ js/Main.js
@@ -1,10 +1,11 @@
 
 import { MostrarHTML } from "./Global.js"
-import { ObtenerDatosConfiguracion } from "./Servicios_Config.js"
+import { ObtenerDatosConfiguracion, ObtenerDatosApiRick } from "./Servicios_Config.js"
+
 /* Los componentes */
 document.addEventListener('DOMContentLoaded', ev => {
     MostrarHTML()
     ObtenerDatosConfiguracion()
-   
+    ObtenerDatosApiRick()
 })
 
--- js/Servicios_Config.js
+++ js/Servicios_Config.js
@@ -23,3 +23,29 @@ export const ObtenerDatosConfiguracion = async () => {
     }
 
 };
+
+export const ObtenerDatosApiRick = async () => {
+
+    let url = "https://rickandmortyapi.com/api/character" 
+
+    try {
+
+        const response = await fetch(url);
+        if (response.status == "200") {
+            const data = await response.json();
+
+            console.warn(data.results);
+            return data.results;
+        }
+        else
+        {
+            console.log("Error al consultar API"+response.status);
+        }
+
+    } catch (error) {
+
+        console.log("Error en la red"+ error);
+
+    }
+
+};

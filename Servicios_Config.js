3c092b465f64a46f18533bba992be27c834e6b8e
Rosita815 <rositajarquin26@gmail.com>
2024-08-06 20:00:04 -0600

    Feat-Entregable_02 Fecha: 06-08-2024

---
+++ Servicios_Config.js
@@ -0,0 +1,24 @@
+export const ObtenerDatosConfiguracion = async () => {
+
+    let url = "/js/Configuraciones.json" 
+
+    try {
+
+        const response = await fetch(url);
+        if (response.status == "200") {
+            const data = await response.json();
+
+            console.log(data);
+        }
+        else
+        {
+            console.log("Error al consultar API"+response.status);
+        }
+
+    } catch (error) {
+
+        console.log("Error en la red"+ error);
+
+    }
+
+};
